fdec35d142609e4dfc1178ca26904a0f320b9fa0
KANTAGBA EFRAIM <efraimkantagba16@gmail.com>
2026-03-25 17:13:41 +0000

    Add request logger middleware in backend

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,12 @@ const app = express();
 // Body parser
 app.use(express.json());
 
+// Logger middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // Enable CORS
 app.use(cors());
 
